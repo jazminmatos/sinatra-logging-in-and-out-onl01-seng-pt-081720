296f4ef9b55850026aade0cab2ebfcb5826f5892
jazminmatos <jmatos1993@gmail.com>
2020-12-16 11:09:21 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+    def self.current_user(session_hash)
+        #use user_id from session hash to find the user in the database & return that user
+        User.find(session_hash[:user_id])
+    end
+
+    def self.is_logged_in?(session_hash)
+        #should return true if user_id is in the session hash
+        #should return false if not
+        #double bang operator - returns boolean
+
+        !!session_hash[:user_id] #shows the boolean value of the statement
+    end
 end
